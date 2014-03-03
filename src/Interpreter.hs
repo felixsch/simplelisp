61b59c877f1c6b0f4a848e775ae3aa8795e034c9
felixsch <felix@fix3d.net>
2014-03-03 10:35:38 +0000

    init

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,10 +1,10 @@
 module Interpreter where
 
+import Control.Applicative
 import Control.Monad.State
 import Control.Monad.Error
 
 import qualified Data.Map as M
-    
 
 import Types
 import Parser
@@ -22,13 +22,14 @@ data Enviroment = Enviroment
 type EnvError = ErrorT String IO
 type Env a = StateT Enviroment EnvError a
 
-newCtx :: Context 
+newCtx :: Context
 newCtx = Context M.empty Nothing
 
 
 findSymbol :: String -> Env LispExp
 findSymbol sym = do
     env <- get
+
     case M.lookup sym (envSym env) of
         Just x -> return x
         Nothing -> findSymbol' (envCtx env)
@@ -37,9 +38,10 @@ findSymbol sym = do
         findSymbol' (Just (Context table ctx)) = case M.lookup sym table of
                                                     Just x -> return x
                                                     Nothing -> findSymbol' ctx
-                        
+
 {-
 (func 2 "moep")
+
 (list 1 2 3 4 5)
 1
 2
@@ -48,6 +50,19 @@ findSymbol sym = do
 (defun (x y) (* x y))
 #t
 -}
+
+concat_ :: [LispExp] -> Env LispExp
+concat_ = foldM cons (LString "")
+    where
+        cons (LString a) (LString b) = return $ LString (a ++ b)
+        cons (LString a)  b          =  throwError $  "Value '" ++ show b ++ "' is not a string."
+
+
+
+
+builtinTable :: M.Map String ([LispExp] -> Env LispExp)
+builtinTable = M.fromList [ ("concat", concat_)]
+
 
 evaluate :: LispExp -> Env LispExp
 evaluate (LSymbol s) = findSymbol s
